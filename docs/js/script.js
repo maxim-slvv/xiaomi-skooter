6556edbd815a318ca33c9f4a0b99911b34c1adac
Maxim <panda.gtr007@gmail.com>
2022-11-17 23:32:17 +0600

    add header and burger menu

---
+++ docs/js/script.js
@@ -0,0 +1,10 @@
+//бургер меню
+$(document).ready(function(){
+    $('.header__burger').click(function(event){
+        $('.header__burger, .header__link, .header__burger-menu').toggleClass('active');
+        //при открытом меню блокируем прокрутку страницы
+        $('body').toggleClass('lock');
+        //если с открытым меню увеличить окно то бургер скроектся
+        if(windowWidth > 767.9)$(".header__burger, .header__link, .header__burger-menu").toggleClass('active');
+    })
+}); 
