57a63700ab5f53daa80e60ff331b4de9cba4575e
Maxim <panda.gtr007@gmail.com>
2022-11-19 18:26:13 +0600

    fixed header

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,21 +1,13 @@
 //бургер меню
 $(document).ready(function(){
     $('.header__burger').click(function(event){
-        $('.header__burger, .header__link, .header__burger-menu, .lbl').toggleClass('active');
+        $('.header__burger, .header__link, .header__burger-menu, .checking').toggleClass('active');
+
         //при открытом меню блокируем прокрутку страницы
         $('body').toggleClass('lock');
+
         //если с открытым меню увеличить окно то бургер скроектся
-        if(windowWidth > 767.9)$(".header__burger, .header__link, .header__burger-menu, .lbl").toggleClass('active');
+        if(windowWidth > 767.9)$(".header__burger, .header__link, .header__burger-menu, .checking").toggleClass('active');
     })
 }); 
 
-$(window).keyup(function(e){
-    var target = $('.checkbox-green input:focus');
-    if (e.keyCode == 9 && $(target).length){
-        $(target).parent().addClass('focused');
-    }
-});
-  
-$('.checkbox-green input').focusout(function(){
-    $(this).parent().removeClass('focused');
-});
