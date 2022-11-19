0dd91077341dc3c3e8613c9285b178ab8bb72fb0
Maxim <panda.gtr007@gmail.com>
2022-11-19 15:56:04 +0600

    fixed background

--- docs/js/script.js
+++ docs/js/script.js
@@ -9,3 +9,13 @@ $(document).ready(function(){
     })
 }); 
 
+$(window).keyup(function(e){
+    var target = $('.checkbox-green input:focus');
+    if (e.keyCode == 9 && $(target).length){
+        $(target).parent().addClass('focused');
+    }
+});
+  
+$('.checkbox-green input').focusout(function(){
+    $(this).parent().removeClass('focused');
+});
