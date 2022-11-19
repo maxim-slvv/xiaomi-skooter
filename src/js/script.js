420336dbddabef3de3a05a859aec82f00e3fb614
Maxim <panda.gtr007@gmail.com>
2022-11-19 12:52:42 +0600

    1

--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,10 @@
 //бургер меню
 $(document).ready(function(){
     $('.header__burger').click(function(event){
-        $('.header__burger, .header__link, .header__burger-menu').toggleClass('active');
+        $('.header__burger, .header__link, .header__burger-menu, .lbl').toggleClass('active');
         //при открытом меню блокируем прокрутку страницы
         $('body').toggleClass('lock');
         //если с открытым меню увеличить окно то бургер скроектся
-        if(windowWidth > 767.9)$(".header__burger, .header__link, .header__burger-menu").toggleClass('active');
+        if(windowWidth > 767.9)$(".header__burger, .header__link, .header__burger-menu, .lbl").toggleClass('active');
     })
 }); 
